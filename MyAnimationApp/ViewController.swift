32bfd5f63e5f002dcababa36f41a221c710cd16f
Yaroslav <yaroslav.olex@gmail.com>
2020-09-19 11:47:26 +0300

    Initial Commit

---
+++ MyAnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAnimationApp
+//
+//  Created by Yaroslav on 19.09.2020.
+//  Copyright © 2020 Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
